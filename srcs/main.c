d201e9c1c64a66cdd3de531a1421be58c1619b76
Ludrak <luca06.robino@gmail.com>
2020-06-20 13:38:05 +0200

    Added basics of command tokenization

---
+++ srcs/main.c
@@ -0,0 +1,84 @@
+#include "minishell.h"
+#include "libft.h"
+#include "get_next_line.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <signal.h>
+
+int main(int ac, char **av)
+{
+    char        *unparsed_cmd;
+    t_cmd_token *token_list;
+    t_cmd_token *tmp;
+    int         notnull;
+
+    printf ("$>");
+    fflush(stdout);
+    while (get_next_line(0, &unparsed_cmd))
+    {
+        token_list = tokenize(unparsed_cmd);
+        notnull = 0;
+        while (token_list)
+        {
+            if (token_list->type == CMD_ARG)
+                printf ("[-- %s]", token_list->value);
+            else if (token_list->type == CMD_EXEC)
+                printf ("[<%s>]", token_list->value);
+            else if (token_list->type == CMD_BUILTIN)
+                printf ("[>%s<]", token_list->value);
+            else if (token_list->type == CMD_PARENT)
+                printf ("[prim exec : ( %s )]", token_list->value);
+            else if (token_list->type == CMD_OP)
+                printf ("{%s}", token_list->value);
+            else
+                printf ("[%s]", token_list->value);
+            if (token_list->next)
+                printf (" ; ");
+            tmp = token_list->next;
+            token_list = tmp;
+            notnull = 1;
+        }
+        destroy_token_list(&token_list);
+
+        if (notnull)
+            printf ("\n");
+        printf ("$>");
+        fflush(stdout);
+        free(unparsed_cmd);
+    }
+    return (0);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+    EXAMPLE FORK PWD
+*/
+/*
+void pwd()
+{
+    if (fork() == 0)
+    {
+        char *argv[] = {"/bin/pwd"};
+        char *newargv[] = { *argv, NULL };
+        char *newenviron[] = { NULL };
+
+        execve(argv[0], newargv, newenviron);
+        kill(0, 0);
+    }
+    else
+        wait(NULL);
+}*/
---
+++ srcs/token/token.c
@@ -0,0 +1,88 @@
+
+/*
+**      -- TOKENIZER --
+**      Includes tokenize(char *raw_command) to split an unparsed command into
+**      a t_cmd_token list.
+*/
+
+#include "token.h"
+
+
+
+t_cmd_token         *tokenize(char *raw_cmd)
+{
+    char        quotes;
+    int         parenthesis;
+    int         i;
+    char        *param;
+    t_cmd_token *token_list;
+    t_cmd_token *first;
+
+    quotes = 0;
+    i = 0;
+    token_list = NULL;
+    param = raw_cmd;
+    parenthesis = 0;
+    if (!*raw_cmd)
+        return (NULL);
+    while (*raw_cmd)
+    {
+        i++;
+        if (*raw_cmd == '(')
+            parenthesis ++;
+        if (*raw_cmd == ')')
+            parenthesis --;
+
+        if (*raw_cmd == QUOTE_SINGLE || *raw_cmd == QUOTE_DOUBLE)
+        {
+            if (quotes == *raw_cmd)
+                quotes = QUOTE_NONE;
+            else
+                quotes = *raw_cmd;
+        }
+ 
+        if (*raw_cmd == ' ' && quotes == QUOTE_NONE && !parenthesis)
+        {
+            if (i != 1)
+            {
+                if (!token_list)
+                {
+                    token_list = create_next_token(NULL, param, i);
+                    first = token_list;
+                }
+                else
+                    token_list = create_next_token(&token_list, param, i);
+            }
+            param = raw_cmd + 1;
+            i = 0;
+        }
+        raw_cmd ++;
+    }
+    if (i != 0)
+    {
+        if (token_list)
+            token_list = create_next_token(&token_list, param, i + 1);
+        else
+            first = create_next_token(NULL, param, i + 1);
+    }
+    //NOTE: IF (QUOTES != NONE) HERE THERE IS AN UNCLOSED QUOTE
+    return (first ? first : 0);
+}
+
+
+
+void            destroy_token_list(t_cmd_token  **list)
+{
+    t_cmd_token *tmp;
+
+    if (list)
+    {
+        while (*list)
+        {
+            tmp = (*list)->next;
+            free((*list)->value);
+            free(*list);
+            *list = tmp;
+        }
+    }
+}
